a5ead8fdbe334309db43cff67e3730bdc224014c
gurunath-patil <gurunathpatil8099@gmail.com>
2026-01-01 12:32:33 +0530

    fix: email id does not truncate correctly & job title goes right side on mobile & tab devices

--- src/components/ContactDetails.vue
+++ src/components/ContactDetails.vue
@@ -25,17 +25,15 @@ let contactDetails = [
 
 </script>
 <template>
-	<div class='grid grid-cols-2 gap-5 p-2 md:ms-0 md:flex md:flex-col md:pb-10 md:gap-6'>
-		<div class='flex overflow-hidden gap-x-2' v-for="item in contactDetails">
-			<div class='bg-[#202022] sm:w-16 w-8 p-1 shadow-lg flex justify-center items-center flex-wrap rounded-xl'>
-				<i :class="item.imgURL" class="text-2xl text-amber-300"></i>
+	<div class='grid grid-cols-2 lg:grid-cols-1 gap-5 p-2 md:ms-0 md:pb-10 md:gap-6'>
+		<div class='grid grid-cols-1 lg:grid-cols-[80px_1fr] gap-y-1 lg:gap-y-0 lg:gap-x-2' v-for="item in contactDetails">
+			<div class='bg-[#202022] w-[55%] lg:w-full col-start-1 col-end-1 lg:row-start-1 lg:row-end-3 p-1 shadow-lg flex justify-center rounded-xl'>
+				<i :class="item.imgURL" class="text-2xl lg:text-3xl text-amber-400"></i>
 			</div>
-			<div class='flex flex-col'>
-				<h4 class='text-xs md:text-base lg:text-base text-orange-300'>{{ item.title }}</h4>
-				<p class='w-25 sm:w-full text-xs md:text-sm lg:text-base text-white font-mono line-clamp-1'>
+				<h4 class='ps-1 text-base lg:text-base text-orange-300'>{{ item.title }}</h4>
+				<p class='ps-1 sm:w-full text-sm lg:text-base text-white font-mono truncate lg:col-start-2 lg:col-end-3'>
 					{{ item.content }}
 				</p>
-			</div>
 		</div>
 	</div>
 </template>
--- src/components/PersonalDetails.vue
+++ src/components/PersonalDetails.vue
@@ -23,8 +23,8 @@ import "../index.css"
 				class="flex overflow-hidden items-center text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-violet-700 text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-7/12 sm:w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2">
 				<span
 					class="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
-				<div class="flex items-center">
-					<span class="ml-1 text-white text-lg ps-3 sm:ms-0">Full Stack Developer</span>
+				<div class="flex items-start md:items-center">
+					<span class="text-white text-lg md:ps-3 sm:ms-0">Full Stack Developer</span>
 				</div>
 			</button>
 		</div>
