174fd1dab0e3fe945f4e41512b8aed48bec2ea3a
gurunath-patil <gurunathpatil8099@gmail.com>
2026-01-29 23:24:18 +0530

    change project sequence

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -6,6 +6,18 @@ import { ref } from 'vue';
 
 const projectDetails = [
 	{
+		projectName: 'YCanary',
+		sourceURL: 'https://ycanary.com/',
+		status: true,
+		imgURL: 'https://ycanary.com/assets/customdashboard-Cl2Mgd-N.webp',
+	},
+	{
+		projectName: 'Weather Forcast',
+		sourceURL: 'https://weather-forcast-nu.vercel.app/',
+		status: true,
+		imgURL: 'https://images.pexels.com/photos/29034990/pexels-photo-29034990/free-photo-of-serene-mountain-view-above-sunrise-clouds.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+	},
+	{
 		projectName: 'Nandan Dairy ERP',
 		imgURL: 'https://www.baramatidoodhsangh.com/new/Home%20Page%20Display-1.webp',
 		status: false,
@@ -18,23 +30,11 @@ const projectDetails = [
 		sourceURL: null,
 	},
 	{
-		projectName: 'YCanary',
-		sourceURL: 'https://ycanary.com/',
-		status: true,
-		imgURL: 'https://ycanary.com/assets/customdashboard-Cl2Mgd-N.webp',
-	},
-	{
 		projectName: 'Bloging Website',
 		sourceURL: 'https://github.com/gurunath-patil/blog-website',
 		status: true,
 		imgURL: 'https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 	},
-	{
-		projectName: 'Weather Forcast',
-		sourceURL: 'https://weather-forcast-nu.vercel.app/',
-		status: true,
-		imgURL: 'https://images.pexels.com/photos/29034990/pexels-photo-29034990/free-photo-of-serene-mountain-view-above-sunrise-clouds.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-	}
 ]
 
 const alertDetails = ref({
